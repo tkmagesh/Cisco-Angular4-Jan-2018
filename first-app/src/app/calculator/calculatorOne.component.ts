69cb1f6ac76e9eb18a42c57e9d50bfdfa61a9602
Magesh <tkmagesh77@gmail.com>
2018-01-09 10:52:52 +0530

    calculator-1 solution - part - 2

--- first-app/src/app/calculator/calculatorOne.component.ts
+++ first-app/src/app/calculator/calculatorOne.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { CalculatorModel } from './CalculatorModel';
 
 @Component({
 	selector : 'calculator-one',
@@ -8,32 +8,6 @@ import { Component } from '@angular/core';
 export class CalculatorOneComponent{
 
 	//state
-	result : number = 0;
-	n1 : number = 0;
-	n2 : number = 0;
 
-	//behavior
-	onN1Change(value){
-		this.n1 = parseInt(value, 10);
-	}
-
-	onN2Change(value){
-		this.n2 = parseInt(value, 10);
-	}
-
-	onAddClick(){
-		this.result = this.n1 + this.n2;
-	}
-
-	onSubtractClick(){
-		this.result = this.n1 - this.n2;
-	}
-
-	onMultiplyClick(){
-		this.result = this.n1 * this.n2;
-	}
-
-	onDivideClick(){
-		this.result = this.n1 / this.n2;
-	}
+	model : CalculatorModel = new CalculatorModel();
 }
