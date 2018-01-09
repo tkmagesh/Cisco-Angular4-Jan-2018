53bef17765a6516858c31cd6645745dd7f2c599b
Magesh <tkmagesh77@gmail.com>
2018-01-09 11:51:08 +0530

    calculator two - solution - 2

--- first-app/src/app/calculator/calculatorTwo.component.ts
+++ first-app/src/app/calculator/calculatorTwo.component.ts
@@ -8,23 +8,6 @@ import { CalculatorModel } from './CalculatorModel';
 export class CalculatorTwoComponent{
 
 	//state
-
 	model : CalculatorModel = new CalculatorModel();
 
-	onCalculateClick(operation){
-		switch (operation) {
-			case "add":
-				this.model.add();
-				break;
-			case "subtract":
-				this.model.subtract();
-				break;
-			case "multiply":
-				this.model.multiply();
-				break;
-			case "divide":
-				this.model.divide();
-				break;
-		}
-	}
 }
