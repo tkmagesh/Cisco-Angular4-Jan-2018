0396e768bce8036ee4f993969a45f2c9141e22b6
Magesh <tkmagesh77@gmail.com>
2018-01-10 12:26:57 +0530

    make the comparer typesafe with IComparer

--- bug-tracker-app/src/app/bugTracker/pipes/sort.pipe.ts
+++ bug-tracker-app/src/app/bugTracker/pipes/sort.pipe.ts
@@ -1,22 +1,27 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+
+interface IComparer{
+	(item1 : any, item2 : any) : number
+}
+
 @Pipe({
 	name : 'sort'
 })
 export class SortPipe implements PipeTransform{
-	private getComparerFor(attrName){
-		return function(item1, item2){
+	private getComparerFor(attrName : string) : IComparer{
+		return function(item1 : any, item2 : any) : number {
 			if (item1[attrName] < item2[attrName]) return -1;
 			if (item1[attrName] > item2[attrName]) return 1;
 			return 0;
 		}
 	}
-	private getDescendingComparerFor(comparer){
-		return function(item1, item2){
+	private getDescendingComparerFor(comparer : IComparer) : IComparer{
+		return function(item1 : any, item2 : any) : number {
 			return comparer(item1, item2) * -1;
 		}
 	}
-	transform(data : any[], attrName : string, isDescending : boolean = false) : any[]{
+	transform(data : any[], attrName : string, isDescending : boolean = false) : any[] {
 		if (!attrName) return data;
 		let comparer = this.getComparerFor(attrName);
 		if (isDescending)
